04da22aef8e0656e2dbafa1afb79703987db8dc5
Jo jae hun <wognskec@naver.com>
2018-12-09 16:32:28 +0900

    URL만들기

---
+++ assets/js/카드뉴스1.js
@@ -0,0 +1,5 @@
+$('#container').tylePlayer([
+  'https://raw.githubusercontent.com/Jaehun22/Card-New-Y-09-monorail-/master/html5up-txt/images/pic02.jpg',
+  'https://d1s0awd0642fry.cloudfront.net/cards.images/eK/eK6faxWbRivP7y5zE_cont.png',
+  'https://d1s0awd0642fry.cloudfront.net/cards.images/Ek/EkM3BqNtd8GPKiX7w_cont.png'
+]);
---
+++ 카드뉴스플레이어1/js/index.js
@@ -0,0 +1,5 @@
+$('#container').tylePlayer([
+  'https://d1s0awd0642fry.cloudfront.net/cards.images/Bz/BzHbuJcmdH39dhtKa_cont.png',
+  'https://d1s0awd0642fry.cloudfront.net/cards.images/eK/eK6faxWbRivP7y5zE_cont.png',
+  'https://d1s0awd0642fry.cloudfront.net/cards.images/Ek/EkM3BqNtd8GPKiX7w_cont.png'
+]);
